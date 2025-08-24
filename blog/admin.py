ff53be0b000479cd839f9102aaac3367cdd63000
MahsaKhazaee22 <mahsakhazaee22m@gmail.com>
2025-08-24 14:38:24 +0330

    admin
    compelet admin

--- blog/admin.py
+++ blog/admin.py
@@ -9,8 +9,8 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
     # fields = ('title',) just shoe title
-    list_display = ('title','counted_views','published_date','created_date','status')
-    list_filter = ('status',)
+    list_display = ('title','author','counted_views','published_date','created_date','status')
+    list_filter = ('status','author',)
     # ordering = ['-created_date']
     search_fields = ['title','content']
 admin.site.register(Post,PostAdmin)
