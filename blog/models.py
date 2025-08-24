93d7173f213ad54cd2cbc7429d7a5c7905ce1032
MahsaKhazaee22 <mahsakhazaee22m@gmail.com>
2025-08-24 11:28:51 +0330

    edit completly
    edit
    temp,url,views,admin,model(blog)
    admin,model(website)

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,10 @@ class Post(models.Model):
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+        # verbose_name  and verbose_name_plural
+
+    def __str__(self):
+        return  " {} - {} ".format(self.title,self.id)
